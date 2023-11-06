b965e6dcfcc1f067b6871c1c5227810a8f21df87
DavideCavallotto <davidecavallotto92@gmail.com>
2023-11-06 14:47:13 +0100

    ADD new project and 3 components

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import AppHeaderPage from './components/AppHeaderPage.vue';
+import AppMainPage from './components/AppMainPage.vue';
+import AppFooterPage from './components/AppFooterPage.vue';
+</script>
+
+<template>
+ 
+</template>
+
+<style lang="scss" scoped>
+
+</style>
