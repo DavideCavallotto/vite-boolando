c19541e1b378aa9d67dbde472e4c9ffb3a56605e
DavideCavallotto <davidecavallotto92@gmail.com>
2023-11-06 16:11:41 +0100

    ADD header page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+
 import AppHeaderPage from './components/AppHeaderPage.vue';
 import AppMainPage from './components/AppMainPage.vue';
 import AppFooterPage from './components/AppFooterPage.vue';
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    AppHeaderPage
+  },
+  methods: {
+    
+
+  }
+}
 </script>
 
 <template>
- 
+ <AppHeaderPage/>
 </template>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+@use './style/general.scss';
 </style>
--- src/components/AppHeaderPage.vue
+++ src/components/AppHeaderPage.vue
@@ -3,7 +3,11 @@
 export default {
   data() {
     return {
-      
+      nav: [
+        'Uomo',
+        'Donna',
+        'Bambino'
+      ],
     }
   }, 
   methods: {
@@ -14,10 +18,58 @@ export default {
 </script>
 
 <template>
-     
+    <div class="page-header">
+        <div class="row-header">              
+            
+            <ul class="menu">
+                <li v-for="items in nav"><a href="#">{{ items }}</a></li>
+                  
 
+            </ul>
+            
+            <div class="boolean-logo">
+                <img src="/img/boolean-logo.png" alt="">
+            </div>
+            <div class="icons">
+                <span class="e-code">&#x1F45C;</span><span class="e-code">&#128100;</span><span class="e-code">&#10084;</span>
+            </div>               
+
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+.page-header {
+    background-color: $header_color;
+
+    .row-header {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+
+    .icons {
+        color: white;
+    }
+
+    .boolean-logo {
+        width: 200px;
+    }
+    .menu {
+    display: flex;
+
+    a {
+        color: white;
+        margin: 0 10px;
+    }
+    
+    }
+}
+   
+}
+
+
 
 </style>
